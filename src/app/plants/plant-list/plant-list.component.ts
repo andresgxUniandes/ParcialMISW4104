fabf94e089a89610846eb8892539b3ddcceba1b4
Andres Gomez H. <ae.gomezh1@uniandes.edu.co>
2025-04-25 13:19:28 -0500

    Add integration with API to get plants

--- src/app/plants/plant-list/plant-list.component.ts
+++ src/app/plants/plant-list/plant-list.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
-
-interface Plant {
-  id: number;
-  nombre: string;
-  tipo: string;
-  clima: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { PlantService } from '../services/plant.service';
 
 @Component({
   selector: 'app-plant-list',
@@ -13,10 +7,14 @@ interface Plant {
   styleUrls: ['./plant-list.component.css'],
   standalone: false
 })
-export class PlantListComponent {
-  plants: Plant[] = [
-    { id: 1, nombre: 'Orquídea', tipo: 'Flor', clima: 'Tropical' },
-    { id: 2, nombre: 'Cactus', tipo: 'Suculenta', clima: 'Árido' },
-    { id: 3, nombre: 'Helecho', tipo: 'Planta de sombra', clima: 'Húmedo' }
-  ];
+export class PlantListComponent implements OnInit {
+  plants: any[] = [];
+
+  constructor(private plantService: PlantService) { }
+
+  ngOnInit(): void {
+    this.plantService.getPlants().subscribe(plants => {
+      this.plants = plants;
+    });
+  }
 }
--- src/app/plants/plants.module.ts
+++ src/app/plants/plants.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
 import { PlantsRoutingModule } from './plants-routing.module';
-
+import { PlantListComponent } from './plant-list/plant-list.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PlantsRoutingModule
+    PlantsRoutingModule,
+    HttpClientModule
   ]
 })
 export class PlantsModule { }
